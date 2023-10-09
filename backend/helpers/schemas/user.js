eaa50435dd6fd3defa655e0bf44f99f410ec94cc
Janessa <lvlajzhao1983@gmail.com>
2023-10-09 12:09:02 +0800

    update schema

--- backend/helpers/schemas/user.js
+++ backend/helpers/schemas/user.js
@@ -3,17 +3,23 @@ const yup = require('yup')
 const schemas = {
     register : yup.object({
         body: yup.object({
-            name : yup.string().min(5).max(15).required(),
-            password: yup.string().min(5).max(10).required(),
+            name : yup.string().min(5).max(15).required('The length of User name must be between 5 and 15'),
+            password: yup
+                        .string().required('Password is required')
+                        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                            'Password should contain at least one letter, one number and minimum eight characters'),
             roles: yup.array().of(yup.string()).min(1).required("Provide at least one role"),
             age: yup.number().min(18).max(80).optional(),
-            email: yup.string().email().optional()
+            email: yup.string().email("Incorrect email format").optional()
         })
     }),
     login : yup.object({
         body: yup.object({
-            name : yup.string().min(5).max(15).required(),
-            password: yup.string().min(5).max(10).required()
+            name : yup.string().min(5).max(15).required('The length of User name must be between 5 and 15'),
+            password: yup
+                .string().required('Password is required')
+                .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    'Password should contain at least one letter, one number and minimum eight characters')
         })
     }),
     getByUserId :yup.object({
